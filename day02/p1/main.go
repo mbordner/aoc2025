e30fd324dd5d7535e55df1bc34e082a766f2eae9
Michael Bordner <mbordner@gmail.com>
2025-12-02 01:35:29 -0500

    day 02

--- day02/p1/main.go
+++ day02/p1/main.go
@@ -1,5 +1,41 @@
 package main
 
+import (
+	"fmt"
+	"github.com/mbordner/aoc2025/common/file"
+	"strconv"
+	"strings"
+)
+
 func main() {
+	idRanges, _ := file.GetContent("../data.txt")
+
+	invalidIds := make(map[string]bool)
+
+	for _, idRange := range strings.Split(string(idRanges), ",") {
+		tokens := strings.Split(idRange, "-")
+		start, _ := strconv.Atoi(strings.TrimSpace(tokens[0]))
+		end, _ := strconv.Atoi(strings.TrimSpace(tokens[1]))
+
+		for i := start; i <= end; i++ {
+			strval := fmt.Sprintf("%d", i)
+			if len(strval)%2 == 0 {
+				firstHalf := strval[0 : len(strval)/2]
+				secondHalf := strval[len(strval)/2:]
+				if firstHalf == secondHalf {
+					invalidIds[strval] = true
+				}
+			}
+		}
+
+	}
+
+	sum := uint64(0)
+	for id := range invalidIds {
+		val, _ := strconv.ParseInt(id, 10, 64)
+		sum += uint64(val)
+	}
+
+	fmt.Println(sum)
 
 }
--- day02/p2/main.go
+++ day02/p2/main.go
@@ -1,5 +1,51 @@
 package main
 
+// package main we'll go through a list of id (number value) ranges, and find invalid ids
+// an invalid id is a number that has repeating number values, e.g. 11, 1212, 123123123
+// it will print out the sum of all the invalid id values in the ranges examined
+
+import (
+	"fmt"
+	"github.com/mbordner/aoc2025/common/file"
+	"strconv"
+	"strings"
+)
+
 func main() {
-	
+	idRanges, _ := file.GetContent("../data.txt")
+
+	sum := uint64(0)
+
+	for _, idRange := range strings.Split(string(idRanges), ",") {
+		tokens := strings.Split(idRange, "-")
+		start, _ := strconv.Atoi(strings.TrimSpace(tokens[0]))
+		end, _ := strconv.Atoi(strings.TrimSpace(tokens[1]))
+
+		for i := start; i <= end; i++ {
+			strVal := fmt.Sprintf("%d", i)
+			h := len(strVal) / 2 // split the string value into two tokens, and store the length of one half
+
+			for l := h; l > 0; l-- { // check token lengths of h down to 1
+				if len(strVal)%l == 0 {
+					token := strVal[0:l] // store first token
+					allMatched := true
+					for t := l; t < len(strVal); t += l { // check each consecutive token of same length l, to see if it matches token
+						checkToken := strVal[t : t+l]
+						if token != checkToken {
+							allMatched = false // we can break out if we don't find a match
+							break
+						}
+					}
+					if allMatched {
+						sum += uint64(i) // if all matched, this is an invalid id, add the value to sum and break
+						break            // we don't need to check any other token length when found to be invalid
+					}
+				}
+			}
+		}
+
+	}
+
+	fmt.Println(sum)
+
 }
